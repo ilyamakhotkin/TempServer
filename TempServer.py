856527b6e8af0ecb8f74f7dec27eb0d12b1f2c0c
Ilya Makhotkin <ilya.makhotkin@gmail.com>
2018-05-01 09:43:44 -0600

    Made to work with Raspberry Pi Modified sensor number Temperature string parsing

--- TempServer.py
+++ TempServer.py
@@ -7,17 +7,18 @@ import socket
 import threading
 import time
 
-sensor = '/sys/bus/w1/devices/222/w1_slave'
+sensor = '/sys/bus/w1/devices/22-00000047ad7c/w1_slave'
 
 
 class TemperatureServer:
     """ docstring """
     def __init__(self):
-        # self.log_file = open("/home/pi/tempServer.log", 'w')
-        self.log_file = open("tempServer.log", 'w')
+        self.log_file = open("/home/pi/tempServer.log", 'w')
+        # self.log_file = open("tempServer.log", 'w')
         self.log_file.write('TempServer started\n')
-        # self.temp_sensor = open(sensor, 'r')
+        self.temp_sensor = open(sensor, 'r')
         self.temp_string = '0.00 C'
+        self.run = True
         # html
         self.html_socket = socket.socket()
         try:
@@ -31,21 +32,22 @@ class TemperatureServer:
 
     def temp_read_loop(self):
         """ loop to read temperatures """
-        temp = 10
-        while True:
-            # data = self.temp_sensor.read()
-            data = str(temp)
-            self.temp_string = data + ' C'
+        while self.run:
+            self.temp_sensor = open(sensor, 'r')
+            data = self.temp_sensor.read()
+            self.temp_sensor.close()
+            data = data.split()
+            data = data[21].split('=')[1]
+            self.temp_string = data[:len(data)-3] + '.' + data[len(data)-3:] + ' C'
             print(self.temp_string)
-            temp += 1
-            time.sleep(1)
+            time.sleep(10)
 
     def html_loop(self):
         """ main loop of server"""
-        while True:
+        while self.run:
             self.html_socket.listen(5)
             connection, address = self.html_socket.accept()
-            self.log_file.write('Connected to ' + address[0] + ':' + str(address[1]))
+            self.log_file.write('Connected to ' + address[0] + ':' + str(address[1]) + '\n')
             received = connection.recv(2000)
             print(received)
             received = received.split(b'\r\n')
@@ -61,18 +63,27 @@ class TemperatureServer:
 
     def html_handler(self):
         string = 'HTTP/1.1 200 OK\r\n'
-        string += 'Date: Mon, 27 Jul 2009 3:13:53 GMT\r\n'
-        string += 'Server: Apache / 2.2.14(Win32)\r\n'
         string += 'Content-Length: 56\r\n'
         string += 'Content-Type: text/html\r\n'
         string += 'Connection: Closed\r\n'
         string += '\r\n'
         string += '<html><body><h1>'
+        string += 'Bedroom temperature = '
         string += self.temp_string
         string += '</h1></body></html>\r\n'
         return string
 
+    def close(self):
+        self.run = False
+        self.log_file.close()
+        self.html_socket.close()
+        self.html_thread.join(5)
+        print('close finished')
 
 
 temp_server = TemperatureServer()
-temp_server.temp_read_loop()
+try:
+    temp_server.temp_read_loop()
+except KeyboardInterrupt:
+    print('Received Interrupt')
+    temp_server.close()
